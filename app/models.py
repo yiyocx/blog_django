e47ed75aee2639e5a47f13e3f6e41e1d5de7132f
Yiyo_PC_Ubuntu <yiyocx@gmail.com>
2013-08-15 13:24:34 -0500

    Agregado el modelo 'Articulo'

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Articulo(models.Model):
+	titulo = models.CharField(max_length=80)
+	contenido = models.TextField()
+	url_img = models.ImageField(upload_to="articulo")
+	fecha = models.DateTimeField(auto_now_add=True)
