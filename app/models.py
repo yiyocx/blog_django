daf01a6bfdd92cf632ecf16a75ed023b7ab97353
xyiyox <yiyocx@gmail.com>
2013-08-16 15:36:09 -0500

    Agregados 2 metodos al modelo Articulo

--- app/models.py
+++ app/models.py
@@ -4,4 +4,16 @@ class Articulo(models.Model):
 	titulo = models.CharField(max_length=80)
 	contenido = models.TextField()
 	url_img = models.ImageField(upload_to="articulo")
-	fecha = models.DateTimeField(auto_now_add=True)
+	fecha = models.DateField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.titulo
+
+	def reciente(self):
+		import datetime as d, time as t
+		a = t.localtime()
+
+		if self.fecha < d.date(a.tm_year, a.tm_mon, a.tm_mday-2):
+			return False
+		else:
+			return True
