fca8700030faf318be47fb5e7e42da198a050f5d
xyiyox <yiyocx@gmail.com>
2013-08-22 10:37:50 -0500

    Actualizados los Modelos de app

--- app/models.py
+++ app/models.py
@@ -1,19 +1,58 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
+
+class Blog(models.Model):
+	nombre = models.CharField(max_length=80)
+	slogan = models.CharField(max_length=150)
+
+	def __unicode__(self):
+		return self.nombre
 
 class Articulo(models.Model):
-	titulo = models.CharField(max_length=80)
+	titulo = models.CharField(max_length=140)
+	autor = models.ForeignKey(User)	
 	contenido = models.TextField()
-	url_img = models.ImageField(upload_to="articulo")
-	fecha = models.DateField(auto_now_add=True)
+	imagen = models.URLField()
+	slug = models.SlugField()
+	fecha = models.DateTimeField(auto_now_add=True)
 
 	def __unicode__(self):
 		return self.titulo
 
-	def reciente(self):
-		import datetime as d, time as t
-		a = t.localtime()
+class Etiqueta(models.Model):
+	nombre = models.CharField(max_length=50)
+	articulo = models.ManyToManyField(Articulo)
 
-		if self.fecha < d.date(a.tm_year, a.tm_mon, a.tm_mday-2):
-			return False
-		else:
-			return True
+	def __unicode__(self):
+		return self.nombre
+
+class Categoria(models.Model):
+	nombre = models.CharField(max_length=50)
+	slug = models.SlugField()
+	padre = models.ForeignKey('Categoria',null=True)
+
+	articulo = models.ManyToManyField(Articulo)	
+
+	def __unicode__(self):
+		return self.nombre
+
+class Comentario(models.Model):
+	titulo = models.CharField(max_length=80)
+
+	articulo = models.ForeignKey(Articulo)
+	usuario = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+
+	#models.
+	#	CASCADE
+	#	PROTECT
+	#	SET_NULL
+	#	SET_DEFAULT / NOTA: Debe existir un valor default=__ 
+	#	DO_NOTHING
+
+	#usuario = models.OneToOneField(User)
+	contenido = models.TextField()
+	fecha = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.titulo
